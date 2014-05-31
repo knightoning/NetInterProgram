092cd2741f433000140d33054e83f043322c7d3b
hujie <hujie20081122@163.com>
2014-05-31 18:09:45 +0800

    修改编码

--- src/com/knight/HelloWorld.java
+++ src/com/knight/HelloWorld.java
@@ -3,8 +3,7 @@ package com.knight;
 public class HelloWorld{
 	
 	public static void main(String[] args){
-		
-	   System.out.println("JAVAÍøÂç±à³Ì¿ªÊ¼......");
+
 	   System.out.println("JAVA Come On.....");
 	}
 	
